3058cba01c312ce2232655c2624374241c71e045
vinod kumar <vinodkmrmurugan@gmail.com>
2013-04-01 09:31:28 +0530

    updated frontend for authentiation
    Added Simple_form, several forms to bootstrap/simple_form
    Added login/logout/Register
    Added user collection

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rake','10.0.4'
 
 gem 'sqlite3'
 gem 'devise'
+gem 'simple_form'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,3 +1,8 @@
 class Status < ActiveRecord::Base
-  attr_accessible :content, :name
+  
+  attr_accessible :content , :user_id 
+  
+  belongs_to :user, :foreign_key=> :user_id
+
+
 end
