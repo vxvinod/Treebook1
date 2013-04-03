330f7aca1ae99fc850715f646f43a9bae2f20c1b
vinod kumar <vinodkmrmurugan@gmail.com>
2013-04-03 23:00:16 +0530

    Added style to Flash message and Validation 2 failure is there in profile_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+
+	def flash_class (type)
+
+		case type
+		when :notice
+			"alert-info"
+		when :alert
+			"alert-error"
+
+		else
+			""
+		end
+	end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "user first name validation " do
+  	user=User.new
+  assert !user.save
+  assert !user.errors[:first_name].empty?
+  end
+  
+  test "user last name validation " do
+  	user=User.new
+  assert !user.save
+  assert !user.errors[:last_name].empty?
+  end
+test "user profile name validation " do
+    user=User.new
+  assert !user.save
+  assert !user.errors[:profile_name].empty?
+  end
+
+  test "validate the profile_name uniqueness" do
+
+    user = User.new
+
+    user.profile_name="vinod"
+
+    
+    assert !user.save
+    
+    assert !user.errors[:profile_name].empty?
+
+end
+
+test "profile name should have correct format without spaces" do
+    user=User.new
+    user.profile_name="Ashok"
+
+    
+    assert !user.save
+    assert !user.errors[:profile_name].empty?
+    assert  user.errors[:profile_name].include?("Must be formatted correctly.")
+
+end
+
+  
+
+
+
 end
